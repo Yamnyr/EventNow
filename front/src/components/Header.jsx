2bf284d479226dc5b8b6b2a8abc29d5f42c1d38a
ikdg0 <isaackmsgit@gmail.com>
2024-05-13 17:12:46 +0200

    Front

---
+++ front/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/esm/Button';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const Header = () => {
+    return (
+        <div>Header</div>
+    );
+}
+
+export default Header;
---
+++ front/src/pages/acceuil/Accueil.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Accueil = () => {
+  return (
+    <>
+      <div>Acceuil</div>
+    </>
+  );
+};
+
+export default Accueil;
