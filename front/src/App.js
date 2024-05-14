03d44161f1b74fbff7fa2e541d922833f1475e25
Yamnyr <quentinwaroquet@gmail.com>
2024-05-14 16:48:42 +0200

    fix

--- front/src/App.js
+++ front/src/App.js
@@ -3,13 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.css"
 import Header from './components/Header';
 import Footer from './components/Footer';
-<<<<<<< HEAD
 import Accueil from './pages/acceuil/accueil';
 import Inscription from './pages/inscription/inscription';
-
-=======
-import Accueil from './pages/acceuil/Accueil'; // Assure-toi que le chemin et le nom du fichier sont corrects
->>>>>>> 592edb6232739e32d8db77d35f7430072c04799d
 import Profil from './pages/profil/profil';
 import Evenement from './pages/evenement/evenement'
 import NewEvenement from './pages/admin/newEvenement'
