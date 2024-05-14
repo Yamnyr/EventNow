1f5d3b346d154dbac6c8dbcc4760cf26be2a7ed3
ikdg0 <isaackmsgit@gmail.com>
2024-05-14 11:58:22 +0200

    navbar fonctionnelle

--- front/src/App.js
+++ front/src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.css"
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Accueil from './pages/acceuil/Accueil'; // Assure-toi que le chemin et le nom du fichier sont corrects
-
+import Accueil from './pages/acceuil/accueil'; // Assure-toi que le chemin et le nom du fichier sont corrects
+import Profil from './pages/profil/profil';
+import Evenement from './pages/evenement/evenement'
 function App() {
   return (
     <Router>
@@ -12,6 +13,8 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Accueil />} />
+          <Route path="/profil" element={<Profil />} />
+          <Route path="/evenement" element={<Evenement />} />
         </Routes>
         <Footer />
       </div>
--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">EventNOW</a>
+                <Link class="navbar-brand" to="/">EventNOW</Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <Link class="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Evénements</a>
+                            <Link class="nav-link" to="/evenement">Evénements</Link>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link" >Profil</a>
+                            <Link class="nav-link" to="profil">Profil</Link>
                         </li>
                     </ul>
                     <form class="d-flex" role="search">
