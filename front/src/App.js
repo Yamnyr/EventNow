c50d763fd1d0c2ad836d00f3b2555bb97f6ab701
ikdg0 <isaackmsgit@gmail.com>
2024-05-14 10:11:30 +0200

    a

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Accueil from './pages/acceuil/Accueil';
 import Footer from './components/Footer';
+import Accueil from './pages/acceuil/Accueil';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Accueil />
-      <Footer />
+    <div>
+      <div className="my-4 pt-4 container-fluid">
+        <Header />
+      </div>
+      <div className="mt-5">
+        <Footer />
+      </div>
+      <div className="col p-0 bg-color-blue-in">
+
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
